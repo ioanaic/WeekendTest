268699cfc18d35d8bf5a007d58b9b35741aa6c05
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-02-25 11:06:22 +0200

    Change sum to suma

--- src/Example.java
+++ src/Example.java
@@ -4,8 +4,8 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("Good bye!");
-        int sum = Calc.add(2, 3);
-        System.out.println(sum);
+        int suma = Calc.add(2, 3);
+        System.out.println(suma);
     }
 
 }
