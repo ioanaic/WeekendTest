56570f6fd51ab2a55b04cceb65723884133ac5d1
FirstJavaProj <crisan_ioana2005@yahoo.com>
2017-02-25 10:54:06 +0200

    First Java program to add 2 numbers

---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Ioana on 2/25/2017.
+ */
+public class Calc {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/Example.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Ioana on 2/25/2017.
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int sum = Calc.add(2, 3);
+        System.out.println(sum);
+    }
+
+}
+
+
+
+
